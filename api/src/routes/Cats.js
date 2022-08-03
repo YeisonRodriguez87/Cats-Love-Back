fe533bf82f9abafd5784bf852c7098368b820e38
Yeison <rodriguezyj.rodriguez@gmail.com>
2022-08-02 23:25:29 -0300

    Comienzo modularización rutas

---
+++ api/src/routes/Cats.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+//const { getIdCat, getAllCats } = require('../controllers/cat');
+
+const router = Router();
+
+
+router.get('/', );
+
+
+
+
+module.exports = router;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const Cats = require('./Cats.js');
+
+
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/cats', Cats);
 
 module.exports = router;
