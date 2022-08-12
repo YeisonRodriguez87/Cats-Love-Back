935c1637a1f5fc61a51feb23bd0a5e76a9530937
Yeison <rodriguezyj.rodriguez@gmail.com>
2022-08-12 10:26:08 -0300

    cambio modelo Cat

--- src/models/Cat.js
+++ src/models/Cat.js
@@ -45,7 +45,7 @@ module.exports = (sequelize) => {
         allowNull: false,
       },
       imagen: {
-        type: DataTypes.TEXT,
+        type: DataTypes.STRING(1234),
         defaultValue:
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPfs7PgLT0vs3qDK3XW__ea-PwwTOO2K8d1Q&usqp=CAU",
       },
